ee75b365f3cf98dfa158640cfb5a3800c9d506fe
AndySpinko <spdale016@gmail.com>
2021-08-06 18:57:55 +0700

    is done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat = applicationContext.getBean(Cat.class);
+        System.out.println(cat.getVoice());
+
+        Cat cat2 = applicationContext.getBean(Cat.class);
+        System.out.println(cat2.getVoice());
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(cat.equals(cat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    public String voice;
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+}
